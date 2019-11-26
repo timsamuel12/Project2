bf2531b292e1cdf06c990d3758dcc359346e55d1
timsamuel12 <timsamuel12@gmail.com>
2019-11-26 18:27:14 -0500

    Set up database file

--- config.php
+++ config.php
@@ -1,1 +1,16 @@
 <?php
+session_start();
+
+define("HOST", "localhost");
+define("USERNAME", "root");
+define("PASSWORD", "root");
+define("DB", "ts553");
+
+try {
+    $pdo = new PDO("mysql:host=" . HOST . ";dbname=" . DB, USERNAME, PASSWORD);
+    // set the PDO error mode to exception
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "Error: " . $e->getMessage();
+    die;
+}
